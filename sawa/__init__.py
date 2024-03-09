224c927c55048eb61ed958e83e0fd3c3d7a78e28
zhang-yige <zyg90086@gmail.com>
2024-03-10 01:34:55 +0800

    update gradio

--- sawa/__init__.py
+++ sawa/__init__.py
@@ -1,2 +1,4 @@
 from .llm import build_llm_from_config
-from .writer import build_writer
+from .writer import Writer, build_writer
+from .retrieval import Retrieval
+from .prompt import RedBookEditorPrompt, WritePrompt
--- tools/gradio_demo.py
+++ tools/gradio_demo.py
@@ -1,39 +1,45 @@
 import gradio as gr
 import yaml
-from sawa import build_writer
+import pandas as pd
+from sawa import Retrieval, WritePrompt, RedBookEditorPrompt, build_llm_from_config
 
-SYSCONFIG = 'config\sawa.yaml'
+title = "小红书文案生成器"
+description = "请输入您想生成小红书文案的话题"
+examples = [
+    ["法国留学攻略，详细介绍需要注意的事项"],
+    ["心理咨询师和心理医生的区别"]
+    ]
+article = "感兴趣的小伙伴可以阅读github项目"
 
-def interface():
-    #标题
-    title = "小红书文案生成器"
-    #题下的描述，支持md格式
-    description = "请输入您想生成小红书文案的话题"
-    #输入样例
-    examples = [
-        ["法国留学攻略，详细介绍需要注意的事项"],
-        ["心理咨询师和心理医生的区别"]
-        ]
-    #页面最后的信息，可以选择引用文章，支持md格式
-    article = "感兴趣的小伙伴可以阅读github项目"
+config_path = 'config\sawa.yaml'
+with open(config_path,'r') as file:
+    cfg = yaml.safe_load(file)
 
-    config_path = SYSCONFIG
-    with open(config_path,'r') as file:
-        cfg = yaml.safe_load(file)
+def xhs_writer(user_query):
+    data = pd.read_csv(cfg['dataset_path'])
+    llm = build_llm_from_config(cfg['openai_config_path'], 
+                                sys_prompter=RedBookEditorPrompt("请协助我完成一系列任务从而完成一篇小红书文案。"))
+    retriever = Retrieval(user_query, cfg['embeddings_path'], llm)
+    info = retriever.semantic_retrieval()
+    index = info.pop('most_similar_document_index')
+    
+    for item in data.columns:
+        info[item] = data[item][index]
 
-    def xhs_writer(user_query):
+    info['user_query'] = user_query
 
-        writer = build_writer(user_query=user_query,**cfg)
-        article = writer.write()
+    prompter = WritePrompt()
+    prompt = prompter(**info)
+    
+    article = llm(prompt)
 
-        return article
+    return prompt, article
 
-    interface = gr.Interface(fn=xhs_writer, 
-                inputs=gr.Textbox(label="请输入文章要求："), 
-                outputs=gr.Textbox(label="结果"), 
-                title=title, description=description, examples=examples, article=article)
+interface = gr.Interface(
+    fn = xhs_writer, 
+    inputs = gr.Textbox(label="请输入或使用下面的examples"), 
+    outputs=[gr.Textbox(label="prompt"),
+            gr.Textbox(label="结果")], 
+    title=title, description=description, examples=examples, article=article)
 
-    interface.launch()
-
-if __name__ == "__main__":
-    interface()
+interface.launch()
