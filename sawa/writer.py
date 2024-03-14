aadbadd3b258ac8a303bb3f99ffa1e398f448792
Hishamew <1176663569@qq.com>
2024-03-14 10:13:26 +0800

    uploaded by hisham

--- sawa/writer.py
+++ sawa/writer.py
@@ -18,7 +18,7 @@ class Writer:
                  openai_config_path = 'config\openai\openai.yaml',
                  dataset_path = 'data/sawa_data_1.csv',
                  embeddings_path = 'data/sawa_data_1.npy',
-                 mode = 'gradio'
+                 mode = None
                  ):
         self.llm = build_llm_from_config(openai_config_path, sys_prompter=RedBookEditorPrompt("请协助我完成一系列任务从而完成一篇小红书文案。"))
         self.retrieval = build_retrieval(user_query,embeddings_path,dataset_path,self.llm)
@@ -57,7 +57,7 @@ def build_writer(user_query,
                  openai_config_path,
                  dataset_path,
                  embeddings_path,
-                 mode,):
+                 mode=None,):
     return Writer(user_query=user_query,
                   openai_config_path=openai_config_path,
                   dataset_path=dataset_path,
